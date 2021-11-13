6dc29fc10ff12c7612a27e689f6aa3214b4f222b
Thomas Toye <thomas@toye.io>
2021-11-13 14:03:33 +0100

    Set up Firebase

--- aanrijroutes-web/src/app/app.module.ts
+++ aanrijroutes-web/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,10 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: ''
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- aanrijroutes-web/src/environments/environment.ts
+++ aanrijroutes-web/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'aanrijroutes-dev',
+    appId: '1:899549819254:web:40dbb77c373a1edbf0ca57',
+    storageBucket: 'aanrijroutes-dev.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBWasv05TKlGskiWQ-3YM2p32K7tjwllu8',
+    authDomain: 'aanrijroutes-dev.firebaseapp.com',
+    messagingSenderId: '899549819254',
+  },
   production: false
 };
 
