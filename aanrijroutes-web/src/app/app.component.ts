e35845b7a94ab81de36c8995a3b056e163f1cb99
Thomas Toye <thomas@toye.io>
2021-11-13 15:22:50 +0100

    Finalize web app route creation

--- aanrijroutes-web/src/app/app.component.ts
+++ aanrijroutes-web/src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { map, Observable } from 'rxjs';
 
-type Marker = {
-  lat: number
-  lng: number
-  draggable: boolean
+const COLLECTION_NAME = 'routes'
+
+type CollectionEntry = {
+  id: string
+  creationDate: string
+  markers: readonly {
+    lat: number
+    lng: number
+  }[]
 }
 
 @Component({
@@ -12,27 +20,108 @@ type Marker = {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(
+    @Inject(MatDialog) public dialog: MatDialog,
+    private afs: AngularFirestore
+  ) {
+    this.collection = this.afs.collection(COLLECTION_NAME)
+    this.items$ = this.collection.valueChanges().pipe(map(list => ([...list]).sort((a, b) => b.creationDate.localeCompare(a.creationDate))))
+  }
+
   title = 'Aanrijroutes';
   lat = 51.05467898167528;
   lng = 3.7257560729602623;
+  items$: Observable<CollectionEntry[]>
+  selectedItem$: Observable<CollectionEntry> | null = null
+  collection: AngularFirestoreCollection<CollectionEntry>
 
-  markers: Marker[] = [
-	  {
-		  lat: 51.05467898167528,
-		  lng: 3.7257560729602623,
-		  draggable: true
-	  }
-  ]
+  mapClicked($event: { coords: { lat: number, lng: number } }, selectedItem: CollectionEntry) {
+    const updated: CollectionEntry = {
+      ...selectedItem,
+      markers: [
+        ...selectedItem.markers,
+        {
+          lat: $event.coords.lat,
+          lng: $event.coords.lng
+        }
+      ]
+    }
 
-  mapClicked($event:  { coords: { lat: number, lng: number } }) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
+    this.collection.doc(selectedItem.id).set(updated)
   }
 
-  markerClicked(i: number) {
-    this.markers = this.markers.filter((_, idx) => idx !== i)
+  markerClicked(i: number, selectedItem: CollectionEntry) {
+    const updated: CollectionEntry = {
+      ...selectedItem,
+      markers: [
+        ...selectedItem.markers.filter((_, idx) => idx !== i)
+      ]
+    }
+
+    this.collection.doc(selectedItem.id).set(updated)
+  }
+
+  changeSelectedRoute(route: CollectionEntry) {
+    console.log('Changed selected route to', route)
+    this.selectedItem$ = this.collection.doc(route.id).valueChanges() as Observable<CollectionEntry>
+  }
+
+  newRouteDialog() {
+    let dialogRef = this.dialog.open(DialogNewRouteDialog, {
+      width: '250px',
+      data: { name: '' }
+    });
+
+    dialogRef.afterClosed().subscribe((result: { name: string }) => {
+      if (result == null || result.name == null || result.name == '') {
+        return
+      }
+
+      const id = result.name.replace(/\s/g, '_')
+
+      const doc: CollectionEntry = {
+        id,
+        creationDate: new Date().toISOString(),
+        markers: [
+          {
+            lat: 51.05467898167528,
+            lng: 3.7257560729602623
+          }
+        ]
+      }
+
+      this.collection.doc(id).set(doc).catch(err => {
+        console.error('Could not create new route', err, doc)
+      }).then(() => {
+        this.selectedItem$ = this.collection.doc(id).valueChanges() as Observable<CollectionEntry>
+      })
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-new-route-name',
+  template: `
+  <h1 mat-dialog-title>Naam voor de nieuwe route</h1>
+  <div mat-dialog-content>
+    <mat-form-field appearance="fill">
+      <mat-label>Naam nieuwe route</mat-label>
+      <input matInput [(ngModel)]="data.name">
+    </mat-form-field>
+  </div>
+  <div mat-dialog-actions>
+    <button mat-button (click)="onNoClick()">Annuleren</button>
+    <button mat-button [mat-dialog-close]="data" cdkFocusInitial>Ok</button>
+  </div>
+`
+})
+export class DialogNewRouteDialog {
+
+  constructor(
+    @Inject(MatDialogRef) public dialogRef: MatDialogRef<DialogNewRouteDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: { name: string }) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
--- aanrijroutes-web/src/app/app.module.ts
+++ aanrijroutes-web/src/app/app.module.ts
@@ -4,26 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogNewRouteDialog } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogNewRouteDialog
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     AgmCoreModule.forRoot({
-      apiKey: ''
+      apiKey: 'AIzaSyBYMmWfBd55HP_5g9j6HobJKNoArE84N5I'
     }),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    BrowserAnimationsModule
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideAuth(() => getAuth()),
+    // provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
